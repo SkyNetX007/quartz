bc1aa1d339b602a3209a44496ea2516d8c487ee8
SkyNetX007 <skynetx007.carnivore@gmail.com>
2023-09-27 17:47:59 +0800

    Keep submodule up to date

--- vercel-submodule-workaround.sh
+++ vercel-submodule-workaround.sh
@@ -27,6 +27,8 @@ cd tmp # go into the tmp folder
 # checkout the current submodule commit
 git init # initialise empty repo
 git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
+# keep submodule up to date
+COMMIT="main"
 git fetch --depth=1 origin $COMMIT # fetch only the required version
 git checkout $COMMIT # checkout on the right commit
 
